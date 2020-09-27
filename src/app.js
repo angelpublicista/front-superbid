89030125151db1bf9951c4131a699c36fc99fe18
Angel Burgos <angelpublicista.1@gmail.com>
2020-09-27 14:50:02 -0500

    Tipo documentos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import $ from "jquery";
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.js';
+import '@fortawesome/fontawesome-free/css/all.css';
 import AOS from 'aos';
 
 // Styles
@@ -13,3 +14,5 @@ import "./static/update-doc.svg";
 import "./static/logo-superbid.svg";
 
 AOS.init();
+
+$('.button-file').tooltip();
