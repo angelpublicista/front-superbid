2fb433f6eb112858fd5ab7508cc501c58076ac60
Angel Burgos <angelpublicista.1@gmail.com>
2020-09-30 10:31:11 -0500

    Campos dinámicos

--- src/helpers/Campos.js
+++ src/helpers/Campos.js
@@ -1,5 +1,5 @@
 class Campos {
-    constructor(contenedor, tipo_campos, nombre_subasta){
+    constructor(contenedor, tipo_campos, nombre_subasta) {
         this.contenedor = contenedor;
         this.tipo_campos = tipo_campos;
         this.nombre_subasta = nombre_subasta;
@@ -8,7 +8,7 @@ class Campos {
         this.fields_box = this.box.querySelector(`.${this.tipo_campos}`);
     }
 
-    crearCampos(){
+    crearCampos() {
         let contenido = ""
         switch (this.tipo_campos) {
             case "campos-registro":
@@ -37,14 +37,150 @@ class Campos {
                             </div>
                         </div>
                     </div>
-                
                 `
                 break;
 
             case "campos-pagos":
-                contenido = `<p>Campos de pagos</p>`
+                contenido = `<div class="sb-docs sb-docs-pagos">
+                                <h3 class="sb-title-head-docs">Documentos de pagos</h3>
+                                <div class="row mt-3">
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-carta-tercero" class="sb-input-file" name="rg-field-carta-tercero">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-carta-tercero" class="mt-2">Carta de tercero</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-soporte-pago-lote" class="sb-input-file" name="rg-field-soporte-pago-lote">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-soporte-pago-lote" class="mt-2">Soporte pago lote</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-soporte-pago-comision" class="sb-input-file" name="rg-field-soporte-pago-comision">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-soporte-pago-comision" class="mt-2">Soporte pago comisión</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-soporte-pago-traspasos" class="sb-input-file" name="rg-field-soporte-pago-traspasos">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-soporte-pago-traspasos" class="mt-2">Soporte pago traspasos</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-soporte-pago-fianza" class="sb-input-file" name="rg-field-soporte-pago-fianza">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-soporte-pago-fianza" class="mt-2">Soporte pago fianza</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
                 break;
-        
+
+            case "campos-garantia":
+                contenido = `<div class="sb-docs sb-docs-garantia">
+                                <h3 class="sb-title-head-docs">Documentos de garantía</h3>
+                                <div class="row mt-3">
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-soporte-garantia" class="sb-input-file" name="rg-field-soporte-garantia">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-soporte-garantia" class="mt-2">Soporte de garantía</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-certificacion-bancaria" class="sb-input-file" name="rg-field-certificacion-bancaria">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-certificacion-bancaria" class="mt-2">Certificación bancaria</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-documentos-garantias" class="sb-input-file" name="rg-field-documentos-garantias">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-documentos-garantias" class="mt-2">Documentos garantias</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
+                break;
+
+            case "campos-retiros":
+                contenido = `<div class="sb-docs sb-docs-retiro">
+                                <h3 class="sb-title-head-docs">Documentos de retiro</h3>
+                                <div class="row mt-3">
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-planilla-aportes" class="sb-input-file" name="rg-field-planilla-aportes">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-planilla-aportes" class="mt-2">Planilla de aportes</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-poliza" class="sb-input-file" name="rg-field-poliza">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-poliza" class="mt-2">Póliza</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-rtm" class="sb-input-file" name="rg-field-rtm">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-rtm" class="mt-2">RTM</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-6 col-md-3">
+                                        <div class="form-group sb-group-file position-relative text-center">
+                                            <input type="file" id="rg-field-soat" class="sb-input-file" name="rg-field-soat">
+                                            <div class="icon-file">
+                                                <i class="fas fa-file"></i>
+                                            </div>
+                                            <label for="rg-field-soat" class="mt-2">SOAT</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
+                break;
+
             default:
                 break;
         }
@@ -52,10 +188,10 @@ class Campos {
         this.fields_box.innerHTML += contenido;
     }
 
-    borrarCampos(){
+    borrarCampos() {
         this.fields_box.innerHTML = "";
     }
-    
+
 }
 
 export default Campos;
--- src/modules/FormReg.js
+++ src/modules/FormReg.js
@@ -7,20 +7,21 @@ const formReg = () => {
     progressBar('#form-register');
     optionFiles('.button-file');
     
-    
     let form_reg = document.querySelector('#form-register');
     const buttons_opt = form_reg.querySelectorAll('.button-file');
-
-    
 
     for (const button of buttons_opt) {
         button.addEventListener('click', function(e) {
             e.preventDefault();
             let id_button = button.getAttribute('id');
             let campos_activar = button.dataset.active;
+            let seccion_subastas = document.querySelector('.datos-subasta');
+            
             let formCampos = new Campos('.sb-ind-files', `${campos_activar}`);
+
             if(isActive(`#${id_button}`)){
                 formCampos.crearCampos();
+                seccion_subastas.classList.remove('section-hide');
             } else {
                 formCampos.borrarCampos();
             }
